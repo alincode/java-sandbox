c2ec0a9f78207de051b5c9c9a57f408b88004cd4
alincode <alin.code@gmail.com>
2015-10-27 18:10:48 +0800

    testNG group test

--- testng-hello-world/src/test/java/idv/AppTest.java
+++ testng-hello-world/src/test/java/idv/AppTest.java
@@ -5,11 +5,21 @@ import org.testng.annotations.Test;
 
 public class AppTest
 {
-    @Test
+    @Test(groups = "Functional")
     public void testGenerate() throws Exception {
         App app = new App();
         String email = app.generate();
         Assert.assertEquals(email, "feedback@yoursite.com");
+    }
+
+    @Test(groups = "Functional")
+    public void test1() throws Exception {
+        Assert.assertEquals(true, true);
+    }
+
+    @Test(groups = "Unit")
+    public void test2() throws Exception {
+        Assert.assertEquals(true, true);
     }
 
 }
